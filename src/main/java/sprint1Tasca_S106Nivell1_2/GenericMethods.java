edad26850fa63ea3442da7e465b1039dfb762e1c
jrffruit518-enrich <jrffruit518@gmail.com>
2025-10-16 12:36:11 +0200

    fix : Change code formatting

--- src/main/java/sprint1Tasca_S106Nivell1_2/GenericMethods.java
+++ src/main/java/sprint1Tasca_S106Nivell1_2/GenericMethods.java
@@ -2,9 +2,9 @@ package sprint1Tasca_S106Nivell1_2;
 
 public class GenericMethods {
 
-    public static <T,U,V> void printArguments(T t ,U u,V v){
-        System.out.println("Arg1: " + (t != null ? t : "null"));
-        System.out.println("Arg2: " + (u != null ? u : "null"));
-        System.out.println("Arg3: " + (v != null ? v : "null"));
+    public static <T,U,V> void printArguments(T t,U u,V v){
+        System.out.println("Arg1: " + (t != null ? t.getClass().getSimpleName() : "null"));
+        System.out.println("Arg2: " + (u != null ? u.getClass().getSimpleName() : "null"));
+        System.out.println("Arg3: " + (v != null ? v.getClass().getSimpleName() : "null"));
     }
 }
