9307b4b7bf556b6bca6a1d80dc94e5468adf9be1
jrffruit518-enrich <jrffruit518@gmail.com>
2025-10-16 11:01:07 +0200

    fix : update the generic method

--- src/main/java/sprint1Tasca_S106Nivell1_2/GenericMethods.java
+++ src/main/java/sprint1Tasca_S106Nivell1_2/GenericMethods.java
@@ -3,6 +3,8 @@ package sprint1Tasca_S106Nivell1_2;
 public class GenericMethods {
 
     public static <T,U,V> void printArguments(T t ,U u,V v){
-        System.out.println(t+"    "+u+"    "+v);
+        System.out.println("Arg1: " + (t != null ? t : "null"));
+        System.out.println("Arg2: " + (u != null ? u : "null"));
+        System.out.println("Arg3: " + (v != null ? v : "null"));
     }
 }
