adb2a141006022bf9f442bc1572b72a8118405a4
jrffruit518-enrich <jrffruit518@gmail.com>
2025-10-16 10:56:56 +0200

    feat : add new class GenericMethods with generic method

---
+++ src/main/java/sprint1Tasca_S106Nivell1_2/GenericMethods.java
@@ -0,0 +1,8 @@
+package sprint1Tasca_S106Nivell1_2;
+
+public class GenericMethods {
+
+    public static <T,U,V> void printArguments(T t ,U u,V v){
+        System.out.println(t+"    "+u+"    "+v);
+    }
+}
