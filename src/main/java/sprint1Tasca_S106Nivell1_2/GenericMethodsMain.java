5bd79b385002f5868deff8837088d412d7b60249
jrffruit518-enrich <jrffruit518@gmail.com>
2025-10-16 11:11:35 +0200

    feat ： add new file GenericMethodsMain

---
+++ src/main/java/sprint1Tasca_S106Nivell1_2/GenericMethodsMain.java
@@ -0,0 +1,13 @@
+package sprint1Tasca_S106Nivell1_2;
+
+public class GenericMethodsMain {
+    public static void main(String[] args) {
+        Person person = new Person("Rong","Jiang",47);
+        GenericMethods.printArguments(person,"hello",5.35);
+        System.out.println();
+        GenericMethods.printArguments("hello",person,5.35);
+        System.out.println();
+        GenericMethods.printArguments(5.35,"hello",person);
+
+    }
+}
