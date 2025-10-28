4c2a169babba4f04b651fbaf9d6d145aa4604f45
jrffruit518-enrich <jrffruit518@gmail.com>
2025-10-28 17:48:23 +0100

    feat : add NoGenericMethodsMain.java fix ： correct NoGenericMethods.java        update README.md

--- src/main/java/sprint1Tasca_S106Nivell1_2/GenericMethodsMain.java
+++ src/main/java/sprint1Tasca_S106Nivell1_2/GenericMethodsMain.java
@@ -3,6 +3,7 @@ package sprint1Tasca_S106Nivell1_2;
 public class GenericMethodsMain {
     public static void main(String[] args) {
         Person person = new Person("Rong","Jiang",47);
+
         GenericMethods.printArguments(person,"hello",5.35);
         System.out.println();
         GenericMethods.printArguments("hello",person,5.35);
