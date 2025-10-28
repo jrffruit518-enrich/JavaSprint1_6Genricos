22bbede1a04c55b883435d625af533485d0046c9
jrffruit518-enrich <jrffruit518@gmail.com>
2025-10-28 13:44:09 +0100

    feat : add README.md and Person2.java

---
+++ src/main/java/sprint1Tasca_S106Nivell2_2/Person2.java
@@ -0,0 +1,52 @@
+package sprint1Tasca_S106Nivell2_2;
+
+import java.util.Objects;
+
+public class Person2 {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person2() {
+    }
+
+    public Person2(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person2 person = (Person2) o;
+        return age == person.age && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
